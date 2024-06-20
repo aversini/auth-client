7585fe45d434559a2e42cef7d1b50c5ba5d08148
Arno V <versini@gmail.com>
2024-06-20 09:15:51 +0200

    minor updates

--- examples/implicit-flow/rsbuild.config.ts
+++ examples/implicit-flow/rsbuild.config.ts
@@ -1,34 +1,14 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
-import fs from "fs-extra";
-
-const packageJson = fs.readJSONSync("package.json");
-
-const buildTime = new Date()
-	.toLocaleString("en-US", {
-		timeZone: "America/New_York",
-		timeZoneName: "short",
-		year: "numeric",
-		month: "2-digit",
-		day: "2-digit",
-		hour: "2-digit",
-		minute: "2-digit",
-	})
-	.replace(/,/g, "");
 
 export default defineConfig({
 	source: {
 		entry: {
 			index: "./src/index.tsx",
 		},
-		define: {
-			"import.meta.env.BUILDTIME": JSON.stringify(buildTime),
-			"import.meta.env.BUILDVERSION": JSON.stringify(packageJson.version),
-		},
 	},
 	output: {
 		polyfill: "off",
-		cleanDistPath: true,
 		distPath: {
 			root: "./dist",
 		},
