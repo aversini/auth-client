3d03af988be13eab5d9cdf6bd124fa0a6530786e
Arno V <versini@gmail.com>
2024-06-29 12:20:22 -0400

    feat: adding decodeToken (#57)

--- packages/auth-common/src/components/verifyToken.ts
+++ packages/auth-common/src/components/verifyToken.ts
@@ -1,4 +1,4 @@
-import { importSPKI, jwtVerify } from "jose";
+import { decodeJwt, importSPKI, jwtVerify } from "jose";
 import { JWT, JWT_PUBLIC_KEY } from "./constants";
 
 export const verifyAndExtractToken = async (token: string) => {
@@ -9,6 +9,14 @@ export const verifyAndExtractToken = async (token: string) => {
 		return await jwtVerify(token, publicKey, {
 			issuer: JWT.ISSUER,
 		});
+	} catch (_error) {
+		return undefined;
+	}
+};
+
+export const decodeToken = (token: string) => {
+	try {
+		return decodeJwt(token);
 	} catch (_error) {
 		return undefined;
 	}
