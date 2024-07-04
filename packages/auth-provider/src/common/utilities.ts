ac32cb0369a50108109f93057a1989042dc51637
Arno V <versini@gmail.com>
2024-07-04 19:03:29 -0400

    fix: refactor getAccessToken to prevent async issues

--- packages/auth-provider/src/common/utilities.ts
+++ packages/auth-provider/src/common/utilities.ts
@@ -216,3 +216,87 @@ export const getAccessTokenSilently = async ({
 		};
 	}
 };
+
+type RefreshTokenResponse = {
+	status: "success" | "failure";
+	newAccessToken?: string;
+	newRefreshToken?: string;
+};
+type RefreshTokenProps = {
+	clientId: string;
+	userId: string;
+	nonce: string;
+};
+export class TokenManager {
+	private refreshTokenPromise: Promise<any> | null = null;
+	private accessToken: string;
+	private refreshToken: string;
+
+	constructor(
+		accessToken: string | null = null,
+		refreshToken: string | null = null,
+	) {
+		this.accessToken = accessToken || "";
+		this.refreshToken = refreshToken || "";
+	}
+
+	async refreshtoken({
+		clientId,
+		userId,
+		nonce,
+	}: RefreshTokenProps): Promise<RefreshTokenResponse> {
+		if (!this.refreshTokenPromise) {
+			// No existing refresh in progress, start a new one
+			this.refreshTokenPromise = this._refreshToken({
+				clientId,
+				userId,
+				nonce,
+			});
+		}
+
+		try {
+			// Wait for the existing refresh or start a new one
+			return await this.refreshTokenPromise;
+		} finally {
+			// Clear the promise to allow subsequent calls
+			this.refreshTokenPromise = null;
+		}
+	}
+
+	private async _refreshToken({
+		clientId,
+		userId,
+		nonce,
+	}: RefreshTokenProps): Promise<RefreshTokenResponse> {
+		const jwtRefresh = await verifyAndExtractToken(this.refreshToken);
+		if (jwtRefresh && jwtRefresh.payload[JWT.USER_ID_KEY] !== "") {
+			// Refresh token is valid, refreshing access token...
+			const response = await getAccessTokenSilently({
+				clientId,
+				userId,
+				nonce,
+				refreshToken: this.refreshToken,
+				accessToken: this.accessToken,
+			});
+			if (response.status) {
+				this.accessToken = response.accessToken;
+				this.refreshToken = response.refreshToken;
+				return {
+					status: "success",
+					newAccessToken: response.accessToken,
+					newRefreshToken: response.refreshToken,
+				};
+			} else {
+				// Access token could not be refreshed, re-authenticate the user...
+				return {
+					status: "failure",
+				};
+			}
+		} else {
+			// Refresh token is not valid, re-authenticate the user...
+			return {
+				status: "failure",
+			};
+		}
+	}
+}
