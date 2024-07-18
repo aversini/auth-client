828089d696284d38fa79fe81af5beee5e2159066
Arno V <versini@gmail.com>
2024-07-18 11:52:31 -0400

    docs: adding debug option at the URL level

--- examples/code-flow/src/index.tsx
+++ examples/code-flow/src/index.tsx
@@ -8,6 +8,7 @@ import { App } from "./main.tsx";
 
 const params = new URL(document.location.href).searchParams;
 const timeout = params.get("timeout") || "90d";
+const debug = Boolean(params.get("debug")) || false;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 			clientId={"b44c68f0-e5b3-4a1d-a3e3-df8632b0223b"}
 			sessionExpiration={timeout}
 			domain={"gizmette.local.com"}
-			debug
+			debug={debug}
 		>
 			<App timeout={timeout} />
 		</AuthProvider>
