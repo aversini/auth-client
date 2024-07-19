418429240c611092080d83c4f217e11b7df38ef6
Arno V <versini@gmail.com>
2024-07-19 09:01:22 -0400

    feat!: adding body as a possible option for getting token
    BREAKING CHANGE: the params are now an object

--- packages/auth-common/src/components/__tests__/getToken.test.ts
+++ packages/auth-common/src/components/__tests__/getToken.test.ts
@@ -1,11 +1,20 @@
-import { type HeadersLike, getToken } from "..";
+import { BodyLike, type HeadersLike, getToken } from "..";
 
 describe("getToken", () => {
 	const clientId = "testClient";
 
 	it("should return empty string if no token is found", () => {
 		const headers: HeadersLike = {};
-		expect(getToken(headers, clientId)).toBe("");
+		expect(getToken({ headers, clientId })).toBe("");
+	});
+
+	it("should extract token from body", () => {
+		const token = "someToken";
+		const headers: HeadersLike = {};
+		const body: BodyLike = {
+			access_token: token,
+		};
+		expect(getToken({ body, headers, clientId })).toBe(token);
 	});
 
 	it("should extract token from authorization header", () => {
@@ -13,7 +22,7 @@ describe("getToken", () => {
 		const headers: HeadersLike = {
 			authorization: `Bearer ${token}`,
 		};
-		expect(getToken(headers, clientId)).toBe(token);
+		expect(getToken({ headers, clientId })).toBe(token);
 	});
 
 	it("should extract token from cookie", () => {
@@ -21,7 +30,7 @@ describe("getToken", () => {
 		const headers: HeadersLike = {
 			cookie: `auth.${clientId}=${token};`,
 		};
-		expect(getToken(headers, clientId)).toBe(token);
+		expect(getToken({ headers, clientId })).toBe(token);
 	});
 
 	it("should prioritize token from cookie over authorization header", () => {
@@ -31,20 +40,34 @@ describe("getToken", () => {
 			authorization: `Bearer ${headerToken}`,
 			cookie: `auth.${clientId}=${cookieToken};`,
 		};
-		expect(getToken(headers, clientId)).toBe(cookieToken);
+		expect(getToken({ headers, clientId })).toBe(cookieToken);
+	});
+
+	it("should prioritize token from body over cookie or authorization header", () => {
+		const headerToken = "headerToken";
+		const cookieToken = "cookieToken";
+		const bodyToken = "bodyToken";
+		const body: BodyLike = {
+			access_token: bodyToken,
+		};
+		const headers: HeadersLike = {
+			authorization: `Bearer ${headerToken}`,
+			cookie: `auth.${clientId}=${cookieToken};`,
+		};
+		expect(getToken({ headers, body, clientId })).toBe(bodyToken);
 	});
 
 	it("should return empty string if authorization header is not in correct format", () => {
 		const headers: HeadersLike = {
 			authorization: "InvalidHeader",
 		};
-		expect(getToken(headers, clientId)).toBe("");
+		expect(getToken({ headers, clientId })).toBe("");
 	});
 
 	it("should return empty string if cookie does not contain the token", () => {
 		const headers: HeadersLike = {
 			cookie: "someOtherCookie=value;",
 		};
-		expect(getToken(headers, clientId)).toBe("");
+		expect(getToken({ headers, clientId })).toBe("");
 	});
 });
