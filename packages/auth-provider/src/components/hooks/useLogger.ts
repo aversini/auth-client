36b91eafff979e0ed3e89b57a587d7d031bea1a6
Arno V <versini@gmail.com>
2024-07-21 20:26:33 -0400

    code review

--- packages/auth-provider/src/components/hooks/useLogger.ts
+++ packages/auth-provider/src/components/hooks/useLogger.ts
@@ -1,12 +1,17 @@
+import { useCallback } from "react";
+
 export const useLogger = (debug?: boolean) => {
 	/**
 	 * This hook is used to log messages to the console. It returns a function
 	 * that can be used to log messages to the console, but it only logs
 	 * messages if the `debug` flag is set to `true`.
 	 */
-	return (...args: unknown[]) => {
-		if (debug) {
-			console.info(`==> [Auth ${Date.now()}]: `, ...args);
-		}
-	};
+	return useCallback(
+		(...args: unknown[]) => {
+			if (debug) {
+				console.info(`==> [Auth ${Date.now()}]: `, ...args);
+			}
+		},
+		[debug],
+	);
 };
