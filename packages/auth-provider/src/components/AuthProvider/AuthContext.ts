5902888efbea88b25d8ea29bd7a309ab45dcb94f
Arno V <versini@gmail.com>
2024-08-19 23:06:49 +0100

    feat!: refactor login processes (#162)
    * feat!: refactor login processes
    
    BREAKING CHANGE: this is dependent on auth-server v8+
    
    * update lock
    
    * Update bundlesize.config.js

--- packages/auth-provider/src/components/AuthProvider/AuthContext.ts
+++ packages/auth-provider/src/components/AuthProvider/AuthContext.ts
@@ -9,7 +9,6 @@ const stub = (): never => {
 export const AuthContext = createContext<AuthContextProps>({
 	isAuthenticated: false,
 	isLoading: false,
-	authenticationType: null,
 	login: stub,
 	logout: stub,
 	getAccessToken: stub,
